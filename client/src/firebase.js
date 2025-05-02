9da19a11a6a5411af8fcb21016aa21454a3af669
nibesh0506 <156560414+nibesh0506@users.noreply.github.com>
2025-05-02 20:33:19 +0545

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyBW2fr1l1IsbwKssS0VgBKxFzGfX3xSa8w",
-  authDomain: "ecom-fe649.firebaseapp.com",
-  projectId: "ecom-fe649",
-  storageBucket: "ecom-fe649.appspot.com",
-  messagingSenderId: "477497849586",
-  appId: "1:477497849586:web:4df2cbf782eb57921f426b",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
